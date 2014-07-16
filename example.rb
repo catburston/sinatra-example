01563abb46416b23591e3d1a1d6273dfeb4eafab
SirArslaan <arhamoja@gmail.com>
2014-07-16 16:37:58 +0200

    First commit

---
+++ example.rb
@@ -0,0 +1,41 @@
+require 'sinatra'
+require 'pry'
+require 'pstore'
+require 'json'
+require 'yaml/store'
+
+store = YAML::Store.new('data.yaml')
+
+get '/' do
+	store.transaction do
+		@tasks=store[:tasks]
+	end
+	erb :data_beta
+end
+
+post '/message' do
+	store.transaction do
+		store[:tasks] ||= []
+		store[:tasks] << params[:message]
+	end
+	redirect to('/')
+end
+
+
+post '/dl' do
+	store.transaction do
+		myd = params[:checkbox].to_i
+		store[:tasks].delete_at(myd)
+	end
+	redirect to('/')
+end
+
+get '/api/items' do
+	# ['test','hello'].to_json
+	tasks=[]
+	store.transaction do
+		tasks=store[:tasks]
+	end
+
+	tasks.to_json
+end
